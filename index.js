895970d52b8120671d696352a9d59c7d8452dc85
inna-cherkashina <166111253+inna-cherkashina@users.noreply.github.com>
2024-07-17 13:06:23 +0700

    сделана первая секция десктопный вариант

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//~ Запуск видеоплеера Start
+let videoElement = document.querySelector('video');
+let buttonVideo = document.querySelector('.btn-video')
+buttonVideo.addEventListener('click', function () {
+  if (videoElement.paused) {
+    videoElement.play();
+    buttonVideo.classList.add('video-pause');
+    buttonVideo.classList.remove('video-play');
+  } else {
+    videoElement.pause();
+    buttonVideo.classList.remove('video-pause');
+    buttonVideo.classList.add('video-play');
+  }
+});
+videoElement.addEventListener('ended', function () {
+  buttonVideo.currentTime = 0;
+  buttonVideo.classList.remove('video-pause');
+  buttonVideo.classList.add('video-play');
+  console.log('video finished');
+}, false);
 
+//~ Запуск видеоплеера End
 
