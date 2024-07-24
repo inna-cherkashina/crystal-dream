1fe96de56d015c7c781ddf472427b4747c59359f
inna-cherkashina <166111253+inna-cherkashina@users.noreply.github.com>
2024-07-24 20:34:47 +0700

    десктопный вариант лэндинга свёрстан

--- index.js
+++ index.js
@@ -21,3 +21,68 @@ videoElement.addEventListener('ended', function () {
 
 //~ Запуск видеоплеера End
 
+//! Слайдер Start
+
+$(document).ready(function () {
+  $('.slider-container').slick({
+    arrows: true,
+    adaptiveHeight: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    swipe: true,
+    touchThreshold: 10,
+    centerMode: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+        }
+      },
+
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        }
+      },
+    ]
+  });
+});
+
+//TODO Аккордион Start
+let accordeons = document.querySelectorAll('.accordion-item');
+accordeons.forEach(function (accord) {
+  accord.addEventListener('click', function (elem) {
+
+    let action = elem.target;
+    let currentAccordElem = action.closest('.accordion-item');//определяем текущий бокс
+    let currentContent = action.nextElementSibling; // находим скрытый контент
+    currentAccordElem.classList.toggle("active"); //присваиваем основному контейнеру флаг активности   
+    if (currentAccordElem.classList.contains("active")) {
+      currentContent.style.maxHeight = currentContent.scrollHeight + "px";
+      //Обратить внимание чтоб стили были присвоены классу .accordion-collapse
+
+    }
+    else {
+      currentContent.style.maxHeight = 0;
+    }
+  });
+});
+//TODO Аккордион End
