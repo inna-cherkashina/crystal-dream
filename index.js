1eb116f2786d82dfae150794ae933702048aa838
inna-cherkashina <166111253+inna-cherkashina@users.noreply.github.com>
2024-07-25 11:43:55 +0700

    Работа над добавлением видеоплееров

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 //~ Запуск видеоплеера Start
 let videoElement = document.querySelector('video');
 let buttonVideo = document.querySelector('.btn-video')
+
+
 buttonVideo.addEventListener('click', function () {
+
   if (videoElement.paused) {
     videoElement.play();
     buttonVideo.classList.add('video-pause');
@@ -11,13 +14,19 @@ buttonVideo.addEventListener('click', function () {
     buttonVideo.classList.remove('video-pause');
     buttonVideo.classList.add('video-play');
   }
+
 });
+
+
 videoElement.addEventListener('ended', function () {
   buttonVideo.currentTime = 0;
   buttonVideo.classList.remove('video-pause');
   buttonVideo.classList.add('video-play');
   console.log('video finished');
 }, false);
+
+
+
 
 //~ Запуск видеоплеера End
 
